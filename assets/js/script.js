50ee46ad103440eb99206dbb69166da6ad2dddea
Ricardo Gava <Ricardoagava@gmail.com>
2021-05-11 00:06:18 -0300

    banner area css remodeled to make possible the slider be functional, this site was made just for learning html and css, it was not thought of having javascript, i'm trying to make this site all functional using my knowledge of this days!

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,49 @@ menuItens.forEach(a => {
     e.target.parentNode.classList.add('active')
   })
 })
-
 activeFirstParentElement(menuItens)
+
+
+// BANNER SLIDER
+const slide = document.querySelectorAll('.banner .slide')
+const sliderDots = document.querySelectorAll('.banner .pointer')
+
+const slideImages = [
+  'url(../../media/bg.jpg)',
+  'url(../../media/foto3.jpg)',
+  'url(../../media/foto6.jpg)'
+]
+
+const addsClassActiveFirstBannerDots = ([firstDot]) =>
+  firstDot.classList.add('active')
+
+const addsClassActiveToSlide = ([firstSlide]) =>
+  firstSlide.classList.add('active-slide')
+
+const removeClassActive = elements =>
+  elements.forEach(el => el.classList.remove('active'))
+
+const removeClassActiveSlide = elements =>
+  elements.forEach(el => el.classList.remove('active-slide'))
+
+sliderDots.forEach((dot, index) => dot.addEventListener('click', e => {
+  removeClassActive(sliderDots)
+  removeClassActiveSlide(slide)
+  e.target.classList.add('active')
+  slide[index].classList.add('active-slide')
+
+  slide[index].style.setProperty('background-image', slideImages[index])
+}))
+
+addsClassActiveFirstBannerDots(sliderDots)
+addsClassActiveToSlide(slide)
+
 
 // OUR PROJECTS NAV
 const filters = document.querySelectorAll('.filters ul li a')
 
 const activeFirstElement = ([firstElement]) =>
   firstElement.classList.add('active')
-
-const removeActiveClass = elements =>
-  elements.forEach(el => el.classList.remove('active'))
 
 filters.forEach(a => {
   a.addEventListener('click', e => {
@@ -33,5 +65,4 @@ filters.forEach(a => {
     e.target.classList.add('active')
   })
 })
-
 activeFirstElement(filters)
